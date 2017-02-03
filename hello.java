cb1ff30f06937aa38731b3335a7788cb175d981f
Erik Cole <coleej@appstate.edu>
2017-02-03 09:17:26 -0500

    Allows argument for user personalization

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class hello {
    public static void main(String[] args) {
+    if(args.length != 0){
+        System.out.println("Hello, " + args[0]);
+    }
+    else{
       System.out.println("Hello, Jay!");
+    }
    }
 }
